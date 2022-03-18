2e46f7b9c39800937916460a71faa072b895a826
szumielxd <43210079+szumielxd@users.noreply.github.com>
2022-03-18 10:32:22 +0100

    Added kyori groupPaths constant

--- portfelx-common-bootstrap/src/main/java/me/szumielxd/portfel/common/loader/CommonDependency.java
+++ portfelx-common-bootstrap/src/main/java/me/szumielxd/portfel/common/loader/CommonDependency.java
@@ -19,19 +19,22 @@ public enum CommonDependency {
 	GSON(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "com,google,code,gson", "gson", "2.9.0", "53fa3e6753e90d931d62cb89580fde2f", "com,google,gson"),
 	RGXGEN(Repository.SONATYPE_SNAPSHOTS, JavaVersionRange.ALL, "com,github,curious-odd-man", "rgxgen", "1.4-SNAPSHOT", "9001282c58fcc6acff13e118ab8c1117", "com,github,curiousoddman"),
 	YAML(Repository.JITPACK, JavaVersionRange.ALL, "me,carleslc,Simple-YAML", "Simple-Yaml", "1.7.3", "cf302a9468e1d16154d93a6cdc763ca9", "org,simpleyaml", "org,yaml"),
-	ADVENTURE_PLATFORM_BUNGEE(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-platform-bungeecord", "4.1.0", "362673bae7a435ba98693485b589c323", new String[0]),
-	ADVENTURE_PLATFORM_BUKKIT(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-platform-bukkit", "4.1.0", "9022357d3878482b183a2943de4ed066", new String[0]),
-	ADVENTURE_PLATFORM_API(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-platform-api", "4.1.0", "a547973483a351b05d011b8dd5082a2d", new String[0]),
-	ADVENTURE_PLATFORM_FACET(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-platform-facet", "4.1.0", "110781c9f639d65deb1dcfd331796f1f", new String[0]),
-	ADVENTURE_TEXT_BUNGEE(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-text-serializer-bungeecord", "4.1.0", "72e0486e4da445bc0bf76155c8f5e4d4", new String[0]),
-	ADVENTURE_TEXT_GSON(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-text-serializer-gson", "4.10.0", "1dba8b6dcf39aec809095785aff98ecf", new String[0]),
-	ADVENTURE_TEXT_GSON_LEGACY(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-text-serializer-gson-legacy-impl", "4.10.0", "b0d567180e32bc9d24d4a18f21dd721d", "net.kyori.adventure.util"),
-	ADVENTURE_TEXT_LEGACY(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-text-serializer-legacy", "4.10.0", "edb9d7c4c875f77d7ab7ddace2fa34ad", "net.kyori.adventure.util"),
-	ADVENTURE_API(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-api", "4.10.0", "d7150507993fe6a0faddcedebeb12897", "net.kyori.adventure.util"),
-	ADVENTURE_NBT(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-nbt", "4.10.0", "960d4803b5e8095e9cee84ef1c7ffa26", new String[0]),
-	ADVENTURE_KEY(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-key", "4.10.0", "86f4b28385b8e921a1db0b5d05c1f403", new String[0]),
-	EXAMINATION_API(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "examination-api", "1.3.0", "b1887361d811c89ccca4dbf61b88def4", "net.kyori.examination"),
+	ADVENTURE_PLATFORM_BUNGEE(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-platform-bungeecord", "4.1.0", "362673bae7a435ba98693485b589c323", "KYORI_RELOCATIONS"),
+	ADVENTURE_PLATFORM_BUKKIT(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-platform-bukkit", "4.1.0", "9022357d3878482b183a2943de4ed066", "KYORI_RELOCATIONS"),
+	ADVENTURE_PLATFORM_API(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-platform-api", "4.1.0", "a547973483a351b05d011b8dd5082a2d", "KYORI_RELOCATIONS"),
+	ADVENTURE_PLATFORM_FACET(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-platform-facet", "4.1.0", "110781c9f639d65deb1dcfd331796f1f", "KYORI_RELOCATIONS"),
+	ADVENTURE_TEXT_BUNGEE(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-text-serializer-bungeecord", "4.1.0", "72e0486e4da445bc0bf76155c8f5e4d4", "KYORI_RELOCATIONS"),
+	ADVENTURE_TEXT_GSON(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-text-serializer-gson", "4.10.0", "1dba8b6dcf39aec809095785aff98ecf", "KYORI_RELOCATIONS"),
+	ADVENTURE_TEXT_GSON_LEGACY(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-text-serializer-gson-legacy-impl", "4.10.0", "b0d567180e32bc9d24d4a18f21dd721d", "KYORI_RELOCATIONS"),
+	ADVENTURE_TEXT_LEGACY(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-text-serializer-legacy", "4.10.0", "edb9d7c4c875f77d7ab7ddace2fa34ad", "KYORI_RELOCATIONS"),
+	ADVENTURE_API(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-api", "4.10.0", "d7150507993fe6a0faddcedebeb12897", "KYORI_RELOCATIONS"),
+	ADVENTURE_NBT(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-nbt", "4.10.0", "960d4803b5e8095e9cee84ef1c7ffa26", "KYORI_RELOCATIONS"),
+	ADVENTURE_KEY(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "adventure-key", "4.10.0", "86f4b28385b8e921a1db0b5d05c1f403", "KYORI_RELOCATIONS"),
+	EXAMINATION_API(Repository.MAVEN_CENTRAL, JavaVersionRange.ALL, "net,kyori", "examination-api", "1.3.0", "b1887361d811c89ccca4dbf61b88def4", "KYORI_RELOCATIONS"),
 	;
+	
+	
+	private final String[] KYORI_RELOCATIONS = { "net.kyori.examination", "net.kyori.adventure.util" }; // a bit like memory waste, but enums don't like static fields used in constructors
 	
 	
 	@Getter private final String downloadPath;
@@ -50,6 +53,17 @@ public enum CommonDependency {
 	
 	private CommonDependency(@NotNull Repository repo, @NotNull JavaVersionRange versionRange, @NotNull String groupId, @NotNull String artifactId, @NotNull String version, @NotNull String md5, @NotNull String... groupPaths) {
 		groupId = groupId.replace(',', '.');
+		for (int i = 0; i < groupPaths.length; i++) {
+			if ("KYORI_RELOCATIONS".equals(groupPaths[i])) {
+				String[] newArray = new String[groupPaths.length + KYORI_RELOCATIONS.length - 1];
+				int j = 0;
+				for (; j < i; j++) newArray[j] = groupPaths[j];
+				j++; // skip actual index
+				for (; j < groupPaths.length; j++) newArray[j] = groupPaths[j];
+				for (String path : KYORI_RELOCATIONS) newArray[++j] = path;
+				groupPaths = newArray;
+			}
+		}
 		this.downloadPath = repo.getUrl() + '/' + groupId.replace('.', '/') + '/' + artifactId + '/' + version + '/';
 		this.repository = repo;
 		this.versionRange = versionRange;
