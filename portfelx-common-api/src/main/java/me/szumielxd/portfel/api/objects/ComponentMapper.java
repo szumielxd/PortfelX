9b6b5237cc13b729fec6e381abf435ddb3113b1b
szumielxd <43210079+szumielxd@users.noreply.github.com>
2024-07-31 03:26:00 +0200

    Fixed missing Component class error

--- portfelx-common-api/src/main/java/me/szumielxd/portfel/api/objects/ComponentMapper.java
+++ portfelx-common-api/src/main/java/me/szumielxd/portfel/api/objects/ComponentMapper.java
@@ -8,13 +8,13 @@ import org.jetbrains.annotations.NotNull;
 
 import com.google.gson.JsonElement;
 
+import lombok.RequiredArgsConstructor;
 import me.szumielxd.legacyminiadventure.LegacyMiniadventure;
 import me.szumielxd.legacyminiadventure.VersionableObject;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 
 public interface ComponentMapper<C> {
-	
 	
 	public @NotNull C jsonToComponent(@NotNull JsonElement json);
 	
@@ -32,22 +32,9 @@ public interface ComponentMapper<C> {
 		return plainText("");
 	}
 	
-	public default @NotNull C kyoriToComponent(@NotNull Component component) {
-		return this.jsonToComponent(GsonComponentSerializer.gson().serializeToTree(component));
-	}
-	
-	public default @NotNull Component componentToKyori(@NotNull C component) {
-		return GsonComponentSerializer.gson().deserializeFromTree(this.componentToJson(component));
-	}
-	
 	public @NotNull C parsePlaceholders(@NotNull C comp, Map<String, C> replacements);
 	
 	public @NotNull C parsePlaceholdersInHover(@NotNull C comp, Map<String, C> replacements);
-	
-	public default @NotNull VersionableObject<C> parseLegacyMessage(@NotNull String message) {
-		return LegacyMiniadventure.get().deserialize(message)
-				.map(this::kyoriToComponent);
-	}
 	
 	public default @NotNull C fullyParsePlaceholders(@NotNull C component, Map<String, C> replacements) {
 		return parsePlaceholders(parsePlaceholdersInHover(component, replacements), replacements);
@@ -60,6 +47,30 @@ public interface ComponentMapper<C> {
 	public default @NotNull VersionableObject<C> replacePlainPlaceholders(@NotNull VersionableObject<C> message, Map<String, String> replacements) {
 		return replacePlaceholders(message, replacements.entrySet().stream()
 				.collect(Collectors.toMap(Entry::getKey, e -> plainText(e.getValue()))));
+	}
+	
+	public default KyoriComponentMapper<C> kyori() {
+		return new KyoriComponentMapper<>(this);
+	}
+	
+	@RequiredArgsConstructor
+	public static class KyoriComponentMapper<C> {
+		
+		private @NotNull ComponentMapper<C> parentMapper;
+		
+		public @NotNull C kyoriToComponent(@NotNull Component component) {
+			return parentMapper.jsonToComponent(GsonComponentSerializer.gson().serializeToTree(component));
+		}
+		
+		public @NotNull Component componentToKyori(@NotNull C component) {
+			return GsonComponentSerializer.gson().deserializeFromTree(parentMapper.componentToJson(component));
+		}
+		
+		public @NotNull VersionableObject<C> parseLegacyMessage(@NotNull String message) {
+			return LegacyMiniadventure.get().deserialize(message)
+					.map(this::kyoriToComponent);
+		}
+		
 	}
 	
 
