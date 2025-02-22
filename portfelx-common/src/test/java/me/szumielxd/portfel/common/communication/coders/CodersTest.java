b54ef09dc97ee9957583275530b724b8011650b4
szumielxd <43210079+szumielxd@users.noreply.github.com>
2025-02-22 22:39:15 +0100

    Some more tests

--- portfelx-common/src/test/java/me/szumielxd/portfel/common/communication/coders/CodersTest.java
+++ portfelx-common/src/test/java/me/szumielxd/portfel/common/communication/coders/CodersTest.java
@@ -1,6 +1,7 @@
 package me.szumielxd.portfel.common.communication.coders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.UUID;
@@ -16,11 +17,12 @@ class CodersTest {
 	
 	
 	@Test
-	void simpleTopMessageConvertion() {
+	void simpleTopMessageConvertionEqual() {
 		var id = UUID.fromString("11111111-2222-3333-4444-555555555555");
 		var player1 = UUID.fromString("00000000-2222-3333-4444-555555555555");
 		var message = new TopMessage(id, new TopUser[] {
-					new TopUser(player1, "player1", 10)
+					new TopUser(player1, "player1", 10),
+					new TopUser(player1, "player2", 1)
 				});
 		var bout = ByteStreams.newDataOutput();
 		PacketCoder.encode(bout, message);
@@ -28,6 +30,36 @@ class CodersTest {
 		var result = PacketCoder.decode(bin, TopMessage.class);
 		assertTrue(result.isPresent());
 		assertEquals(message, result.get());
+	}
+	
+	@Test
+	void simpleTopMessageConvertionEmptyEqual() {
+		var id = UUID.fromString("11111111-2222-3333-4444-555555555555");
+		var message = new TopMessage(id, new TopUser[] {});
+		var bout = ByteStreams.newDataOutput();
+		PacketCoder.encode(bout, message);
+		var bin = ByteStreams.newDataInput(bout.toByteArray());
+		var result = PacketCoder.decode(bin, TopMessage.class);
+		assertTrue(result.isPresent());
+		assertEquals(message, result.get());
+	}
+	
+	@Test
+	void simpleTopMessageConvertionNotEqual() {
+		var id = UUID.fromString("11111111-2222-3333-4444-555555555555");
+		var player1 = UUID.fromString("00000000-2222-3333-4444-555555555555");
+		var message = new TopMessage(id, new TopUser[] {
+					new TopUser(player1, "player1", 10)
+				});
+		var notMessage = new TopMessage(id, new TopUser[] {
+				new TopUser(player1, "player", 10)
+			});
+		var bout = ByteStreams.newDataOutput();
+		PacketCoder.encode(bout, message);
+		var bin = ByteStreams.newDataInput(bout.toByteArray());
+		var result = PacketCoder.decode(bin, TopMessage.class);
+		assertTrue(result.isPresent());
+		assertNotEquals(notMessage, result.get());
 	}
 	
 
