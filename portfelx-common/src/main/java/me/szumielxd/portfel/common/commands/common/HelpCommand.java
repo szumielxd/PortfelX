653685a99c880bb294d8ea7e3cca57d007755c11
szumielxd <43210079+szumielxd@users.noreply.github.com>
2025-03-28 07:39:16 +0100

    Fixed command logic for arguments

--- portfelx-common/src/main/java/me/szumielxd/portfel/common/commands/common/HelpCommand.java
+++ portfelx-common/src/main/java/me/szumielxd/portfel/common/commands/common/HelpCommand.java
@@ -1,6 +1,5 @@
 package me.szumielxd.portfel.common.commands.common;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -10,7 +9,6 @@ import org.jetbrains.annotations.NotNull;
 import me.szumielxd.portfel.api.Portfel;
 import me.szumielxd.portfel.api.objects.CommonSender;
 import me.szumielxd.portfel.common.commands.CmdArg;
-import me.szumielxd.portfel.common.commands.ParentCommand;
 import me.szumielxd.portfel.common.commands.SimpleCommand;
 import me.szumielxd.portfel.common.lang.Lang.LangKey;
 import me.szumielxd.portfel.common.lang.MainLangKey;
@@ -25,20 +23,17 @@ public class HelpCommand<C> extends SimpleCommand<C> {
 
 	@Override
 	public void onCommand(@NotNull CommonSender<C> sender, @NotNull ParsedCommandContext parsedContext) {
-		String[] label = parsedContext.label();
-		String[] shortLabel = Arrays.copyOf(label, label.length - 1);
 		Portfel<C> pl = this.getPlugin();
 		MainLangKey.COMMAND_MAIN_RUNNING
 				.draft(pl.toString())
 				.sendPrefixed(sender);
-		if (!label[shortLabel.length].equals("")) {
-			ParentCommand<C> parent = (ParentCommand<C>) this.getParent();
-			parent.getChildrens().stream()
+		if (parsedContext.argsLeft().length > 0) {
+			getParent().getChildrens().stream()
 					.sorted(Comparator.comparing(SimpleCommand::getName, String.CASE_INSENSITIVE_ORDER))
 					.forEachOrdered(
 							cmd -> MainLangKey.COMMAND_MAIN_SUBCOMMANDS_LINE
 									.draft(
-											"/" + String.join(" ", shortLabel) + " " + cmd.getName(),
+											"/" + String.join(" ", parsedContext.label()) + " " + cmd.getName(),
 											cmd.getAllArgs().stream()
 													.map(CmdArg::asDraft)
 													.map(MessageDraft.space()::append)
@@ -52,7 +47,7 @@ public class HelpCommand<C> extends SimpleCommand<C> {
 		} else {
 			MainLangKey.COMMAND_MAIN_USE
 					.draft(
-							"/" + String.join(" ", shortLabel) + " help",
+							"/" + String.join(" ", parsedContext.label()) + " help",
 							MessageDraft.array(
 									MainLangKey.COMMAND_SUBCOMMANDS_EXECUTE,
 									MessageDraft.newline(),
